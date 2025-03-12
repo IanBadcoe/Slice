74fb64a7b88485f124b3858c5499c3e0ac3d3c02
Ian Badcoe <igbadcoe@gmail.com>
2025-03-12 21:39:24 +0000

    more experimentation

--- TextBlock.cs
+++ TextBlock.cs
@@ -3,4 +3,10 @@ using System;
 
 public partial class TextBlock : RichTextLabel
 {
+    public override void _Ready()
+    {
+        // var new_sb = new StyleBoxFlat();
+        // new_sb.BgColor = Color.FromHsv(0.5f, 0.5f, 0.5f);
+        // AddThemeStyleboxOverride("normal", new_sb);
+    }
 }
